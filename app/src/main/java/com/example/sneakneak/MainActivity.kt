7e1a857bdf630491a34085f2ccde439984987d78
theun1c <belovdima1122@yandex.ru>
2026-03-13 13:38:33 +0300

    [update](ui): update main act - delete separated funcs

--- app/src/main/java/com/example/sneakneak/MainActivity.kt
+++ app/src/main/java/com/example/sneakneak/MainActivity.kt
@@ -1,40 +1,16 @@
 package com.example.sneakneak
 
 import android.os.Bundle
-import android.text.style.BackgroundColorSpan
-import android.view.animation.OvershootInterpolator
-import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.example.sneakneak.ui.navigation.Navigation
 import com.example.sneakneak.ui.theme.SneakneakTheme
-import kotlinx.coroutines.delay
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,62 +26,5 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Navigation(){
-    val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "splash_screen") {
-        // Splash screen
-        composable("splash_screen") {
-            SplashScreen(navController = navController)
-        }
 
-        // Main screen
-        composable("main_screen") {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ){
-                Text(text = "Main Screen",
-                    color = Color.Black,
-                    fontSize = 24.sp
-                )
-            }
-        }
-    }
-}
-
-// Splash UI
-@Composable
-fun SplashScreen(navController: NavController) {
-
-    val scale = remember {
-        androidx.compose.animation.core.Animatable(0f)
-    }
-
-    // Animation Effect
-    LaunchedEffect(key1 = true) {
-        scale.animateTo(
-            targetValue = 0.7f,
-            animationSpec = tween(
-                durationMillis = 800,
-                easing = {
-                    OvershootInterpolator(4f).getInterpolation(it)
-                }
-            )
-        )
-        delay(1200L)
-        navController.navigate("main_screen")
-    }
-
-    // Img
-    Box(contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-            .background(color = Color(0xFF48B2E7)),
-    ){
-        Image(painter = painterResource(id = R.drawable.logo_foreground),
-            contentDescription = "Logo",
-            modifier = Modifier.scale(scale.value)
-                .size(200.dp))
-    }
-}
