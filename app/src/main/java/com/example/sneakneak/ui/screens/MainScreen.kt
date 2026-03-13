26312fda4ce7d138090a1dee5d300000cfa90944
theun1c <belovdima1122@yandex.ru>
2026-03-13 13:37:32 +0300

    [feat](ui): separate main and splash screens

---
+++ app/src/main/java/com/example/sneakneak/ui/screens/MainScreen.kt
@@ -0,0 +1,24 @@
+package com.example.sneakneak.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun MainScreen(navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = "Main Screen",
+            color = Color.Black,
+            fontSize = 24.sp
+        )
+    }
+}
