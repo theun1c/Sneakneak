d0cc565f9d92889f1ae0a166fc39914059ec16ca
theun1c <belovdima1122@yandex.ru>
2026-03-13 13:36:33 +0300

    [feat](navigation): made navigation a separate function

---
+++ app/src/main/java/com/example/sneakneak/ui/navigation/Navigation.kt
@@ -0,0 +1,32 @@
+package com.example.sneakneak.ui.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.sneakneak.ui.screens.MainScreen
+import com.example.sneakneak.ui.screens.SplashScreen
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "splash_screen") {
+        // Splash screen
+        composable("splash_screen") {
+            SplashScreen(navController = navController)
+        }
+
+        // Main screen
+        composable("main_screen") {
+            MainScreen(navController = navController)
+        }
+    }
+}
